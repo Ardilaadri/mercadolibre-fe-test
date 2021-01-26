d5f45508daf9b81a7bf84e133b031800c27ddc7e
Adriana Ardila <adriana2014lucia@outlook.com>
2021-01-25 19:38:13 -0500

    Add History template

---
+++ mercadolibre-fe-test/src/components/History.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "../assets/styles/components/History.scss"
+
+const History = () => (
+    <section class="history">
+      <div class="history-container">
+        <ul>
+          <li>[Parámetro 1]</li>
+          <li>></li>
+          <li>[Parámetro 2]</li>
+          <li>></li>
+          <li>[Parámetro 3]</li>
+          <li>></li>
+          <li>[Parámetro 4]</li>
+          <li>></li>
+          <li>[Parámetro 5]</li>
+        </ul>
+      </div>
+    </section>
+);
+
+export default History;

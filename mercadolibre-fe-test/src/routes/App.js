07417e2587a4fa11db03a27a310fa4fcfa32febb
Adriana Ardila <adriana2014lucia@outlook.com>
2021-01-27 17:23:25 -0500

    Add Routes

--- mercadolibre-fe-test/src/routes/App.js
+++ mercadolibre-fe-test/src/routes/App.js
@@ -1,11 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Searcher from "../components/Searcher";
+import Products from "../components/Products";
+import Results from "../components/Results";
 import "../assets/styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Searcher />
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Route path="/" component={Searcher} />
+          <Route path="/items?search=" component={Products} />
+          <Route path="/items/:id" component={Results} />
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
